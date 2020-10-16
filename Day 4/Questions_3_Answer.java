82d047da23981451574af7f389d6f054a16fd74f
Kevin Bhingaradiya <37360392+kevinbhingaradiya@users.noreply.github.com>
2020-10-16 12:11:41 +0530

    Create Questions_3_Answer.java

---
+++ Day 4/Questions_3_Answer.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Questions_3_Answer {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Values: ");
+        
+        int a[] = new int[5];
+        int sum = 0;
+        
+        for(int i=0; i<5; i++){
+            a[i] = sc.nextInt();
+        }
+        
+        for(int i=0; i<5; i++){
+            sum = sum + a[i];
+        }
+        
+        System.out.println("Sum of the Entered Values are: "+sum);
+        
+    }
+    
+}
