5efbf7f3b65d93eeb4a7f287958302e52a828450
Kevin Bhingaradiya <37360392+kevinbhingaradiya@users.noreply.github.com>
2020-10-16 12:10:38 +0530

    Create Questions_2_Answer.java

---
+++ Day 4/Questions_2_Answer.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Questions_2_Answer {
+
+    public static void main(String[] args) {
+        
+        int a[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 5 Integer Values: ");
+        
+        for(int i=0; i<5; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        System.out.println("Odd Numbers are: ");
+        
+        for(int i=0; i<5; i++){
+            if(a[i]%2!=0){
+                System.out.println(a[i]+" ");
+            }
+            
+        }
+        
+    }
+    
+}
