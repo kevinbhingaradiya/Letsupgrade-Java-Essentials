1e0d1cca5762f956f194f0ba1e9fe3fe9a4fffb8
Kevin Bhingaradiya <37360392+kevinbhingaradiya@users.noreply.github.com>
2020-10-20 10:09:47 +0530

    Create Main.java

---
+++ Day 5/Main.java
@@ -0,0 +1,13 @@
+package Quiz_Game;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        quiz_game game=new quiz_game();
+        game.initGame();
+        game.play();
+
+    }
+    
+}
