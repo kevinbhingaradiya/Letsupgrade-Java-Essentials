803500f1c019b5660cbab46cee9139a093fc0d9f
Kevin Bhingaradiya <37360392+kevinbhingaradiya@users.noreply.github.com>
2020-10-20 10:08:55 +0530

    Create Player.java

---
+++ Day 5/Player.java
@@ -0,0 +1,18 @@
+package Quiz_Game;
+
+import java.util.Scanner;
+
+public class Player {
+    
+    Scanner sc=new Scanner(System.in);
+    String name;
+    int score=0;
+    
+    public void getDetails() {
+        
+        System.out.println("Enter Player Name : ");
+        name=sc.next();
+    
+    }
+    
+}
